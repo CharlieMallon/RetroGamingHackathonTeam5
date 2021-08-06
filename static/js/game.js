88d1278b468b6924cf07984a00d686b7c2e6c741
CharlieMallon <penguin47@hotmail.co.uk>
2021-08-06 17:46:07 +0100

    added start scene with background

--- static/js/game.js
+++ static/js/game.js
@@ -1,11 +1,32 @@
-// initialize kaboom context
-const k = kaboom();
+kaboom({
+	...{
+		fullscreen: false,
+		width: 550,
+		height: 400,
+		scale: 2,
+		startScene: 'main',
+		version: '0.5.0',
+	},
+	global: true,
+	plugins: [peditPlugin, asepritePlugin, kbmspritePlugin],
+});
 
-// add a text of size 32 at position (100, 100)
-k.add([
-    k.text("oh hi", 32),
-    k.pos(100, 100),
-]);
+// load background sprites (Main Game)
+loadSprite('sky', 'placeholders/screen/sky_area.png')
+loadSprite('ground', 'placeholders/screen/ground_area.png')
+loadSprite('upgrade', 'placeholders/screen/upgrade_area.png')
 
-// or
-// k.addText("oh hi", 32, { pos: k.vec2(100, 100) });
+// Main Game Scene
+scene('main', (args = {}) => {
+    layers([
+        'bg',
+        'game',
+        'ui'
+    ], 'game')
+    add([sprite('sky'), layer('bg'), pos(0, 0),scale(1)])
+    add([sprite('ground'), layer('bg'), pos(0, 226),scale(1)])
+    add([sprite('upgrade'), layer('bg'), pos(0, 350)])
+});
+
+// Start the game on loading
+start('main');
