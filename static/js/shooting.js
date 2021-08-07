6e1e01d8dc2157db69c24ea62fd0aa4574c2956e
Danhayes3008 <43347669+Danhayes3008@users.noreply.github.com>
2021-08-07 12:17:25 +0100

    added a sprite when user clicks on the screen

--- static/js/shooting.js
+++ static/js/shooting.js
@@ -1,10 +1,12 @@
 const shooting = () => {
+    const MISSLE_SPEED = 300;
+    loadSprite('mark', 'sprites/mark.png');
+
     const position = mouseClick(() => {
 	    const mpos = mousePos();
 	    console.log(mpos);
+        add([sprite('mark'), layer('ui'), pos(250, 200)]);        
 	});
 }
-    
-    
 
 export default shooting
