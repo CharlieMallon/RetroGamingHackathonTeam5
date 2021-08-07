3c0e02e944549d7283d4a2cfe38da92f06af2566
psan <pablo.xez@gmail.com>
2021-08-07 22:02:17 +0100

    add missile line and remove missile mark

--- static/js/shooting.js
+++ static/js/shooting.js
@@ -4,7 +4,8 @@ loadSprite('mark', 'sprites/mark.png');
 
 const MISSLE_SPEED = 100;
 const SHOOT_ORIGIN = vec2(275, 300);
-
+var canShoot = true;
+var shootRefresh = 2.0;
 
 
 const shooting = () => {
@@ -12,13 +13,12 @@ const shooting = () => {
         var mPos = mousePos()
         console.log(mPos)
         const missile = add([
-            sprite('mark'),
+            //sprite('mark'),
             pos(SHOOT_ORIGIN),
             origin('center'),
             'missile',
         ])
-    
-        // action(missile, (m) => {
+
         missile.action(() => {
             var move_vec = vec2((mPos.x - SHOOT_ORIGIN.x), (mPos.y - SHOOT_ORIGIN.y))
             var move_vec_mag = move_vec.len();
@@ -26,19 +26,28 @@ const shooting = () => {
         
             missile.move(vec2(move_vec_norm.x * MISSLE_SPEED, move_vec_norm.y * MISSLE_SPEED))
           
+            // render(() => {
+            //     drawLine(SHOOT_ORIGIN, missile.pos)
+            // });
             render(() => {
-              drawLine(SHOOT_ORIGIN, missile.pos)
+                if (missile.pos.y > mPos.y){
+                    drawLine(SHOOT_ORIGIN, missile.pos)
+                }
             });
-           
+            
+
             if (missile.pos.y < mPos.y){
                 destroy(missile)
 
                 // Instantiate explosion here
                 // destroy the line
+            }else{
+                
             }
             console.log("running")
-
         })
+
+        
 	});    
 }
 
