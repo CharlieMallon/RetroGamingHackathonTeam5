574515f109e8e8e27f270935a45ac08ed813a3df
psan <pablo.xez@gmail.com>
2021-08-07 13:54:31 +0100

    add move vector to player shot

--- static/js/shooting.js
+++ static/js/shooting.js
@@ -2,7 +2,8 @@
 
 const shooting = () => {
     // shooting specifications
-    const MISSLE_SPEED = 1;
+    const MISSLE_SPEED = 50;
+    const SHOOT_ORIGIN = vec2(250, 200)
     // loads a sprite
     loadSprite('mark', 'sprites/mark.png');
 
@@ -10,15 +11,33 @@ const shooting = () => {
         // gets the mouse position when the user clicks on the screen
 	    const mpos = mousePos();
 	    console.log(mpos);
+        var move_vec = vec2((mpos.x - SHOOT_ORIGIN.x), (mpos.y - SHOOT_ORIGIN.y))
+
+            var move_vec_mag = move_vec.len();
+           
+            var move_vec_norm = vec2(move_vec.x/move_vec_mag, move_vec.y/move_vec_mag)
         // adds a sprite when user clicks on the screen
-        add([sprite('mark'), layer('ui'), pos(250, 200), "mark"]);
+        add([sprite('mark'), layer('ui'), pos(SHOOT_ORIGIN.x, SHOOT_ORIGIN.y), "mark"]);
         // makes the sprite move
         const move = action('mark', (m) => {
-            var x = mpos.x - 250;
-            var y = mpos.y - 250;
-            // var nrom = new Vec2(x, y)
-            m.move(vec2(10, -10));
-            // m.move(mpos.x, mpos.y);
+            console.log("MOVE");
+            
+            
+            // var x = mpos.x - 250;
+            // var y = mpos.y - 250;
+            // // var nrom = new Vec2(x, y)
+            // m.move(vec2(10, -10));
+            // // m.move(mpos.x, mpos.y);
+            
+            
+
+            
+            console.log("Magnitude: " + move_vec_mag + " | Normalized vec2.x: " + move_vec_norm.x + "  vec2.y:" );
+  
+
+             m.move(vec2(move_vec_norm.x * MISSLE_SPEED, move_vec_norm.y * MISSLE_SPEED));
+
+
         });
 	});    
 }
