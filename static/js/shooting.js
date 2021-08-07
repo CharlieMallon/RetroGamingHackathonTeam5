8b32810a9de3c280d233a0599b50d022e30a5a09
psan <pablo.xez@gmail.com>
2021-08-07 16:01:22 +0100

    fix shooting.js now spawns a smiley towards mouse position

--- static/js/shooting.js
+++ static/js/shooting.js
@@ -1,44 +1,31 @@
-// Work in progress
+import kaboom from './kaboom.js'
+
+loadSprite('mark', 'sprites/mark.png');
+
+const MISSLE_SPEED = 50;
+const SHOOT_ORIGIN = vec2(250, 200);
+
+
 
 const shooting = () => {
-    // shooting specifications
-    const MISSLE_SPEED = 50;
-    const SHOOT_ORIGIN = vec2(250, 200)
-    // loads a sprite
-    loadSprite('mark', 'sprites/mark.png');
-
     const position = mouseClick(() => {
-        // gets the mouse position when the user clicks on the screen
-	    const mpos = mousePos();
-	    console.log(mpos);
-        var move_vec = vec2((mpos.x - SHOOT_ORIGIN.x), (mpos.y - SHOOT_ORIGIN.y))
-
-            var move_vec_mag = move_vec.len();
-           
-            var move_vec_norm = vec2(move_vec.x/move_vec_mag, move_vec.y/move_vec_mag)
-        // adds a sprite when user clicks on the screen
-        add([sprite('mark'), layer('ui'), pos(SHOOT_ORIGIN.x, SHOOT_ORIGIN.y), "mark"]);
-        // makes the sprite move
-        const move = action('mark', (m) => {
-            console.log("MOVE");
-            
-            
-            // var x = mpos.x - 250;
-            // var y = mpos.y - 250;
-            // // var nrom = new Vec2(x, y)
-            // m.move(vec2(10, -10));
-            // // m.move(mpos.x, mpos.y);
-            
-            
-
-            
-            console.log("Magnitude: " + move_vec_mag + " | Normalized vec2.x: " + move_vec_norm.x + "  vec2.y:" );
+        var mPos = mousePos()
   
-
-             m.move(vec2(move_vec_norm.x * MISSLE_SPEED, move_vec_norm.y * MISSLE_SPEED));
-
-
-        });
+        const missile = add([
+            sprite('mark'),
+            pos(SHOOT_ORIGIN),
+            origin('center'),
+            'missile',
+        ])
+    
+        // action(missile, (m) => {
+        missile.action(() => {
+            var move_vec = vec2((mPos.x - SHOOT_ORIGIN.x), (mPos.y - SHOOT_ORIGIN.y))
+            var move_vec_mag = move_vec.len();
+            var move_vec_norm = vec2(move_vec.x/move_vec_mag, move_vec.y/move_vec_mag)
+        
+            missile.move(vec2(move_vec_norm.x * MISSLE_SPEED, move_vec_norm.y * MISSLE_SPEED))
+        })
 	});    
 }
 
