82eee508abe54f30b5d97872d8c934da9d57be8a
Danhayes3008 <43347669+Danhayes3008@users.noreply.github.com>
2021-08-07 12:41:26 +0100

    added comments to the code

--- static/js/shooting.js
+++ static/js/shooting.js
@@ -1,11 +1,16 @@
 const shooting = () => {
+    // shooting specifications
     const MISSLE_SPEED = 300;
+    // loads a sprite
     loadSprite('mark', 'sprites/mark.png');
 
     const position = mouseClick(() => {
+        // gets the mouse position when the user clicks on the screen
 	    const mpos = mousePos();
 	    console.log(mpos);
+        // adds a sprite when user clicks on the screen
         add([sprite('mark'), layer('ui'), pos(250, 200), "mark"]);
+        // makes the sprite move
         const move = action('mark', (m) => {
             m.move(0, -MISSLE_SPEED);
         });
