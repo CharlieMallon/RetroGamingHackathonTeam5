9dba8d0b5855ba1d9936956030a011859600a574
psan <pablo.xez@gmail.com>
2021-08-07 16:49:05 +0100

    add line renderers to missile position

--- static/js/shooting.js
+++ static/js/shooting.js
@@ -2,15 +2,15 @@ import kaboom from './kaboom.js'
 
 loadSprite('mark', 'sprites/mark.png');
 
-const MISSLE_SPEED = 50;
-const SHOOT_ORIGIN = vec2(250, 200);
+const MISSLE_SPEED = 100;
+const SHOOT_ORIGIN = vec2(275, 300);
 
 
 
 const shooting = () => {
     const position = mouseClick(() => {
         var mPos = mousePos()
-  
+        console.log(mPos)
         const missile = add([
             sprite('mark'),
             pos(SHOOT_ORIGIN),
@@ -25,6 +25,22 @@ const shooting = () => {
             var move_vec_norm = vec2(move_vec.x/move_vec_mag, move_vec.y/move_vec_mag)
         
             missile.move(vec2(move_vec_norm.x * MISSLE_SPEED, move_vec_norm.y * MISSLE_SPEED))
+
+          
+            render(() => {
+                drawLine(SHOOT_ORIGIN, missile.pos)
+            });
+            // Draw a line from SHOOT_ORIGIN to missile.position
+           
+            if (missile.pos.y < mPos.y){
+                
+
+                destroy(missile)
+               
+                // Instantiate explosion here
+            }
+            console.log("running")
+
         })
 	});    
 }
