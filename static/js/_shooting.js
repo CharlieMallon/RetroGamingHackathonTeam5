91faedea4b6d5a864835a81576c2ae5f61429236
Paul Harrington <hello@loosenthedark.tech>
2021-08-08 21:40:09 +0100

    Remove ref to deleted sprite from _shooting.js

--- static/js/_shooting.js
+++ static/js/_shooting.js
@@ -13,22 +13,20 @@ import kaboom from './kaboom.js'
 //     },
 // ]);
 
-loadSprite('mark', 'sprites/mark.png');
-
 const MISSLE_SPEED = 50;
 const SHOOT_ORIGIN = vec2(250, 200);
 
-function shoot_missile(from, to){
+function shoot_missile(from, to) {
     add([
-        sprite('mark'), 
-        layer('ui'), 
-        pos(SHOOT_ORIGIN.x, SHOOT_ORIGIN.y), 
+        sprite('mark'),
+        layer('ui'),
+        pos(SHOOT_ORIGIN.x, SHOOT_ORIGIN.y),
         'missile'
     ])
 }
 
 keyPress('space', () => {
-    shoot_missile(SHOOT_ORIGIN, vec2(0,0))
+    shoot_missile(SHOOT_ORIGIN, vec2(0, 0))
 })
 
 
@@ -36,24 +34,24 @@ const shooting = () => {
     // shooting specifications
 
     // loads a sprite
-   
+
 
     const position = mouseClick(() => {
         // gets the mouse position when the user clicks on the screen
-	    // var mpos = mousePos();
-	   
+        // var mpos = mousePos();
+
         // var move_vec = vec2((mpos.x - SHOOT_ORIGIN.x), (mpos.y - SHOOT_ORIGIN.y))
         // var move_vec_mag = move_vec.len();
         // var move_vec_norm = vec2(move_vec.x/move_vec_mag, move_vec.y/move_vec_mag)
-  
+
         // add([sprite('mark'), layer('ui'), pos(SHOOT_ORIGIN.x, SHOOT_ORIGIN.y), "mark"]);
-  
+
         // var move = action('mark', (m) => {
         //     console.log("MOVE");
 
         //     m.move(vec2(move_vec_norm.x * MISSLE_SPEED, move_vec_norm.y * MISSLE_SPEED));
         //     });
-	});    
+    });
 }
 
 export default shooting
