a42d5035c07667b54cfab7c90ddd443f2e5326f8
psan <pablo.xez@gmail.com>
2021-08-08 09:37:34 +0100

    add speed upgrade button

--- static/js/upgrades.js
+++ static/js/upgrades.js
@@ -1,5 +1,6 @@
 
 loadSprite("explosion_upgrade", "sprites/upgrades/explosion.png");
+loadSprite("speed_upgrade", "sprites/upgrades/speed.png");
 
 const upgrades = () => {
     var upgradePos = [
@@ -11,16 +12,34 @@ const upgrades = () => {
         vec2 (350, 375),
     ]
 
-    upgradePos.forEach(element => {
-        add([
-            sprite("explosion_upgrade"),
-            "explosion_upgrade",
-            pos(element),
-            origin('center'),
-            scale(.1),
+    // upgradePos.forEach(element => {
+    //     add([
+    //         sprite("explosion_upgrade"),
+    //         "explosion_upgrade",
+    //         pos(element),
+    //         origin('center'),
+    //         scale(.1),
            
-        ]);
-    }) 
+    //     ]);
+    // }) 
+
+    add([
+        sprite("explosion_upgrade"),
+        "explosion_upgrade",
+        pos(upgradePos[0]),
+        origin('center'),
+        scale(.1),
+       
+    ]);
+
+    add([
+        sprite("speed_upgradeg"),
+        "speed_upgrade",
+        pos(upgradePos[1]),
+        origin('center'),
+        scale(.1),
+       
+    ]);
 }
 
 export default upgrades
