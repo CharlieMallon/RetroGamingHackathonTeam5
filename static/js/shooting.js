9a29210233c5d8e4bd01fbf61ec30efd6c8f9f7e
psan <pablo.xez@gmail.com>
2021-08-08 07:45:52 +0100

    add explotion to player missile and now destroys light ufos

--- static/js/shooting.js
+++ static/js/shooting.js
@@ -1,46 +1,76 @@
 import kaboom from './kaboom.js'
 
 loadSprite('mark', 'sprites/mark.png');
+loadSprite('explosion', 'sprites/explosion.png')
 
-const MISSLE_SPEED = 100;
+const MISSLE_SPEED = 150;
 const SHOOT_ORIGIN = vec2(275, 300);
-var canShoot = true;
-var shootRefresh = 2.0;
+var shootRefresh = 1000;
 
+var millisLast = 0;
+var millis = 0;
+
+var salvagedParts = 0;
 
 const shooting = () => {
     const position = mouseClick(() => {
-        var mPos = mousePos()
-        console.log(mPos)
-        const missile = add([
-            //sprite('mark'),
-            pos(SHOOT_ORIGIN),
-            origin('center'),
-            'missile',
-        ])
-
-        missile.action(() => {
-            var move_vec = vec2((mPos.x - SHOOT_ORIGIN.x), (mPos.y - SHOOT_ORIGIN.y))
-            var move_vec_mag = move_vec.len();
-            var move_vec_norm = vec2(move_vec.x/move_vec_mag, move_vec.y/move_vec_mag)
-        
-            missile.move(vec2(move_vec_norm.x * MISSLE_SPEED, move_vec_norm.y * MISSLE_SPEED))
-          
-            render(() => {
-                if (missile.pos.y > mPos.y){
-                    drawLine(SHOOT_ORIGIN, missile.pos)
-                }
-            });
+        millis = Date.now() - millisLast;
+        if (millis > shootRefresh){
+            millisLast = Date.now()
+            var mPos = mousePos()
+            const missile = add([
+                //sprite('mark'),
+                pos(SHOOT_ORIGIN),
+                origin('center'),
+                'missile',
+            ])
             
+            missile.action(() => {
+                //calculate vector from shoot origin to mPos
+                var move_vec = vec2((mPos.x - SHOOT_ORIGIN.x), (mPos.y - SHOOT_ORIGIN.y))
+                var move_vec_mag = move_vec.len();
+                var move_vec_norm = vec2(move_vec.x/move_vec_mag, move_vec.y/move_vec_mag)
+                
+                //move the missile from from shoot origin to mPos
+                missile.move(vec2(move_vec_norm.x * MISSLE_SPEED, move_vec_norm.y * MISSLE_SPEED))
+                
+                //draw missile line
+                render(() => {
+                    if (missile.pos.y > mPos.y){
+                        drawLine(SHOOT_ORIGIN, missile.pos)
+                    }
+                });
 
-            if (missile.pos.y < mPos.y){
-                destroy(missile)
+                //When missile goes past click pos
+                if (missile.pos.y < mPos.y){
+                    //shake cam
+                    camShake(2);
 
-                // Instantiate explosion here
-            }
-        })
+                    //instantiate explosion
+                    const explosion = add([
+                        sprite('explosion'),
+                        pos(mPos),
+                        origin('center'),
+                        'explosion'
+                    ])
 
-        
+                    explosion.collides('light', (l) => {
+                        destroy(l);
+                    });
+
+                    //destroy the missile object
+                    destroy(missile)
+
+                    //destroy the explosion after a delay
+                    setTimeout(function(){ 
+                        destroy(explosion); 
+                    }, 1000);
+                }
+            })
+
+    }else{
+        console.log("cant shoot")
+    }
 	});    
 }
 
