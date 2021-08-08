b68bce345241f265495e3339450cdcc708bd90aa
psan <pablo.xez@gmail.com>
2021-08-08 09:30:49 +0100

    change sprite for explosion upgrade and place upgrade buttons on screen

--- static/js/upgrades.js
+++ static/js/upgrades.js
@@ -1,6 +1,25 @@
 
+loadSprite("explosion_upgrade", "sprites/upgrades/explosion.png");
 
 const upgrades = () => {
+    var upgradePos = [
+        vec2 (50, 375),
+        vec2 (110, 375),
+        vec2 (170, 375),
+        vec2 (230, 375),
+        vec2 (290, 375),
+        vec2 (350, 375),
+    ]
+
+    upgradePos.forEach(element => {
+        add([
+            sprite("explosion_upgrade"),
+            "explosion_upgrade",
+            pos(element),
+            origin('center'),
+            scale(.1),
+        ]);
+    }) 
 }
 
 export default upgrades
